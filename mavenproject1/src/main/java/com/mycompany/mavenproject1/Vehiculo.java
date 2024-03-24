c342198f388aa454392d555315eed5c14728d327
UzzielAlfaro <164286432+UzzielAlfaro@users.noreply.github.com>
2024-03-23 23:16:29 -0600

    Update Vehiculo.java

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Vehiculo.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Vehiculo.java
@@ -1,6 +1,21 @@
-
 package com.mycompany.mavenproject1;
 
 public class Vehiculo {
+
+    protected String marcaC;
+    protected String colorC;
+
+    public Vehiculo (String marcaC, String colorC){
+        this.colorC = colorC;
+        this.marcaC = marcaC;
+    }
+
+    public String getMarcaC() {
+        return marcaC;
+    }
+
+    public String getColorC() {
+        return colorC;
+    }
     
 }
