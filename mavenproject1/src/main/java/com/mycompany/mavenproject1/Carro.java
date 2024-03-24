21fdcdf0dbf1db9058dc253cd04029d430b6ebaf
PabloRCZ <164437119+PabloRCZ@users.noreply.github.com>
2024-03-23 22:21:01 -0600

    Update Carro.java

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Carro.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Carro.java
@@ -1,6 +1,36 @@
-
 package com.mycompany.mavenproject1;
 
-public class Carro {
-    
+import java.util.Scanner;
+
+public class Carro extends Vehiculo {
+
+    static void carro() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private String gasolinaC;
+
+    public Carro(String marcaC, String colorC, String gasolinaC) {
+        super(marcaC, colorC);  
+        this.gasolinaC = gasolinaC;
+    }
+
+    public void setgasolinaC(String gasolinaC) {
+        this.gasolinaC = gasolinaC;
+    }
+
+    public String getgasolinaC() {
+        return gasolinaC;
+    }
+
+    public void ingresarDatosVehiculo() {
+        Scanner mdc = new Scanner(System.in);
+
+        System.out.print("¿De que marca es su vehiculo?");
+        marcaC = mdc.nextLine();
+        System.out.print("¿De que color es su vehiculo?");
+        colorC = mdc.nextLine();
+        System.out.print("¿Que tipo de gasolina usa su carro?");
+	gasolinaC = mdc.nextLine();
+    }
 }
