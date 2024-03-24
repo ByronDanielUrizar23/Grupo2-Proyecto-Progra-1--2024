70da19f65559abfb8d1a3afa00a911f1f8222fa7
ByronDanielUrizar23 <163693552+ByronDanielUrizar23@users.noreply.github.com>
2024-03-23 22:21:14 -0600

    Update Balsa.java

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Balsa.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Balsa.java
@@ -1,6 +1,34 @@
-
 package com.mycompany.mavenproject1;
 
-public class Balsa {
+public class Balsa extends Vehiculo {
+
+    static void balsa() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private boolean tieneMotor;
     
+    public Balsa(String marcaC, String colorC, boolean tieneMotor) {
+        super(marcaC, colorC);
+        this.tieneMotor = tieneMotor;
+    }
+        
+
+    // Método para obtener el método de movimiento
+    public String obtenerMetodoMovimiento() {
+        if (tieneMotor) {
+            return "motor";
+        } else {
+            return "remo";
+        }
+    }
+
+    // Método para establecer si tiene motor
+    public void setTieneMotor(boolean tieneMotor) {
+        this.tieneMotor = tieneMotor;
+    }
+ // Método para verificar si tiene motor
+    public boolean tieneMotor() {
+        return tieneMotor;
+    }
 }
